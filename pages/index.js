5e526b7f4213e650a812ef623990436f2c928952
romulofh <romulofurtadoo@gmail.com>
2022-01-26 16:05:21 -0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function PaginaInicial() {
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundColor: appConfig.theme.colors.primary[300],
           backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
